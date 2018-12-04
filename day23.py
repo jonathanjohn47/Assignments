6eaed99e497ec3c8114defededa8e2c7d978f8a5
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2018-12-04 13:26:10 +0530

    day 23 python assignment
    Q5 not working for some reason

--- day23.py
+++ day23.py
@@ -3,6 +3,46 @@ import numpy as np
 from pandas import DataFrame as f
 from pandas import Series as s
 
+ar = np.arange(1,17).reshape(4,4)
+print(ar)
+
+#-------------------------------------
+
+mar = np.ma.masked_array(ar, mask = ar%3== 0)
+
+#-------------------------------------
+
+far = f(mar)
+
+print(far)
+
+#-------------------------------------
+
+sar = s(np.arange(1,17))
+sar2 = np.ma.masked_array(sar, mask = sar%3 == 0)
+print(sar2)
+sar3 = s(sar2)
+
+sar3.fillna(method = 'ffill', inplace = True)
+print(sar3)
+
+sar3.fillna(method = 'bfill', inplace = True)
+print(sar3)
+
+
+#-------------------------------------
+
+f1 = f(ar, index = list('a b c d'.split()), columns = 'c1 c2 c3 c4'.split())
+f2 = f1.reindex(list('abcde'))
+f2 = f2.reindex(columns = 'c1 c2 c3 c4 c5'.split())
+print(f1)
+print(f2)
+
+f3 = f2.reindex(list('abcde'), method = 'ffill')
+print '========================'
+print(f3)
+
+#-------------------------------------
 
 ar = np.arange(1,17).reshape(4,4)
 
@@ -56,3 +96,4 @@ print(s2)
 res1 = s1.subtract(s2, fill_value = 0)
 res2 = s2.subtract(s1, fill_value = 0)
 print(res1, res2)
+
